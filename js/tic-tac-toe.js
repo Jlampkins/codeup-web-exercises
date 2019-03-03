19951b2d471a9eadee425b98547109237a0adb1e
Jlampkins <Jlampkins42@gmail.com>
2019-03-02 21:59:49 -0600

    added mobile responsiveness

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
    $(".box").on("click",function(){
        if($(this).text()==="" && play){     /** checks to see if the board can be claimed **/
            if(move % 2 === 1) {             /** checks to see how many moves have been made **/
-               $(this).html("<h1>X</h1>");  /** if move is odd an X is placed.  If even an O is placed **/
+               $(this).html("<h2>X</h2>");  /** if move is odd an X is placed.  If even an O is placed **/
            }else {
-               $(this).append("<h1>O</h1>")
+               $(this).append("<h2>O</h2>")
            }
            move++;
            if (checkForWinner()!== -1 && checkForWinner() !== "") {
@@ -50,15 +50,15 @@ $(document).ready(function(){
 
     function checkForWinner() {
         /** Grabbing what is entered in the box **/
-        var space1 = $("#board .row:nth-child(1) .box:nth-child(1)").text();
-        var space2 = $("#board .row:nth-child(1) .box:nth-child(2)").text();
-        var space3 = $("#board .row:nth-child(1) .box:nth-child(3)").text();
-        var space4 = $("#board .row:nth-child(2) .box:nth-child(1)").text();
-        var space5 = $("#board .row:nth-child(2) .box:nth-child(2)").text();
-        var space6 = $("#board .row:nth-child(2) .box:nth-child(3)").text();
-        var space7 = $("#board .row:nth-child(3) .box:nth-child(1)").text();
-        var space8 = $("#board .row:nth-child(3) .box:nth-child(2)").text();
-        var space9 = $("#board .row:nth-child(3) .box:nth-child(3)").text();
+        var space1 = $("#board .row:nth-child(2) .box:nth-child(1)").text();
+        var space2 = $("#board .row:nth-child(2) .box:nth-child(2)").text();
+        var space3 = $("#board .row:nth-child(2) .box:nth-child(3)").text();
+        var space4 = $("#board .row:nth-child(3) .box:nth-child(1)").text();
+        var space5 = $("#board .row:nth-child(3) .box:nth-child(2)").text();
+        var space6 = $("#board .row:nth-child(3) .box:nth-child(3)").text();
+        var space7 = $("#board .row:nth-child(4) .box:nth-child(1)").text();
+        var space8 = $("#board .row:nth-child(4) .box:nth-child(2)").text();
+        var space9 = $("#board .row:nth-child(4) .box:nth-child(3)").text();
         /** checking rows for match **/
         if      ((space1 === space2) && (space2 === space3)) { return space3; }
         else if ((space4 === space5) && (space5 === space6)) { return space6; }
