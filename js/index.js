e20c2cb45763928ce1708a6e32f09b63690d565c
Jlampkins <Jlampkins42@gmail.com>
2019-03-11 16:59:59 -0500

    import and require

---
+++ js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/** import **/
+import {niceMessage} from "./say-hello.js"
+
+console.log(niceMessage());
+
+
+
+
+// const messageTwo = require("./say-hello.js");
+// console.log(messageTwo());
---
+++ js/say-hello.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/** import import **/
+    export const niceMessage = function() {
+    return "You so good lookin ;)";
+};
+
+/** import require **/
+// const messageTwo = function(){
+//     return "what is this garbage"
+// };
+//
+// module.exports = messageTwo;
+
